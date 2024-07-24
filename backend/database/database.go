2ee42485aa312c2c09945b6bffe417d9c09212dd
CrazyTim71 <118295691+CrazyTim71@users.noreply.github.com>
2024-07-24 16:20:58 +0200

    add db handler

---
+++ backend/database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+var MongoDB *mongo.Database
+
+func InitDatabase() error {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Println("You must set your 'MONGODB_URI' environmental variable.")
+		return errors.New("You must set your 'MONGODB_URI' environmental variable.")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	dbName := os.Getenv("MONGODB_DBNAME")
+	if dbName == "" {
+		log.Println("The 'MONGODB_DBNAME' environmental variable is not set. Defaulting to 'TimeCraft'.")
+		dbName = "ComicCollector"
+	}
+	MongoDB = client.Database(dbName)
+	return nil
+}
