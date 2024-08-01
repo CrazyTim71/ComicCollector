f531eeaa9fc55081ae2bbb8a1a5b81d2956a3e2d
CrazyTim71 <118295691+CrazyTim71@users.noreply.github.com>
2024-08-01 22:36:34 +0200

    first working webui
    Took 30 minutes

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"ComicCollector/main/backend/database"
-	"ComicCollector/main/backend/env"
 	"ComicCollector/main/backend/router"
 	"ComicCollector/main/backend/utils/crypt"
+	"ComicCollector/main/backend/utils/env"
 	"embed"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -14,12 +14,17 @@ import (
 var Files embed.FS
 
 func main() {
+	// TODO: check the log level & why is the output red in Goland ??
+	// TODO: set the log level of gin based on a env flag
+	log.Println("\nComicCollector" + "\nVersion: " + env.VERSION)
+
 	// read the embedded files
 	_, err := Files.ReadDir("main/frontend")
 	if err != nil {
 		log.Println("Failed to read the frontend files")
 		return
 	}
+	env.Files = Files
 
 	// init the environment
 	env.InitEnvironment()
--- main/backend/router/router.go
+++ main/backend/router/router.go
@@ -2,12 +2,27 @@ package router
 
 import (
 	v1 "ComicCollector/main/backend/api/v1"
+	"ComicCollector/main/backend/utils/env"
 	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
 )
 
 func InitRouter(r *gin.Engine) bool {
 	apiV1 := r.Group("/api/v1")
 	v1.ApiHandler(apiV1)
+
+	r.StaticFileFS("/favicon.ico", "main/frontend/static/favicon.ico", http.FS(env.Files))
+
+	r.GET("/login", func(c *gin.Context) {
+		template := template.Must(
+			template.ParseFS(
+				env.Files,
+				"main/frontend/public/login/index.gohtml",
+				"main/frontend/templates/base.gohtml"))
+
+		template.Execute(c.Writer, nil)
+	})
 
 	return true
 }
